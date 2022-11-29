e85c9af1b7fda52de6322cca9af43cb47a2345da
livghit <liviu.g01@gmail.com>
2022-11-29 20:34:40 +0100

    Redone Layout

--- src/components/Layout/Layout.server.jsx
+++ src/components/Layout/Layout.server.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import { Link } from '@shopify/hydrogen'
 
 function Layout({ children }) {
   return (
     <>
-      <header className="bg-white shadow-lg">Layout Navbar</header>
+      <header className="bg-white shadow-lg p-5 text-black">
+        <Link to='/'>
+          <img src="./logoEGJ.png" alt="Logo" className='h-14 w-12' />
+        </Link>
+        <button className='btn'>press</button>
+        <div className='flex flex-col'>
+          <Link to='/'>Armbänder</Link>
+          <Link to='/'>Ringe</Link>
+          <Link to='/'>Ohrringe</Link>
+          <Link to='/'>Halsketten</Link>
+          <Link to='/'>Sets</Link>
+        </div>
+      </header>
 
       <main role={"main"} id="mainContent" className="flex-grow">
         {children}
